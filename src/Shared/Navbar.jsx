7c9dfcd00b36e716de3ade804329953ab15da0f4
iinaamasum <iinaamasum@gmail.com>
2022-05-25 22:10:31 +0600

    navbar fixed with other changes

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = ({ children }) => {
     { id: 4, path: '/dashboard', name: 'Dashboard' },
   ];
   return (
-    <section>
+    <section className="">
       <div class="drawer drawer-end">
         <input id="drawer-open-close" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col">
-          <div class="w-full bg-base-300">
+        <div class="drawer-content flex flex-col relative">
+          <div class="w-full bg-base-300 fixed top-0 left-0 right-0 z-50">
             <div
               style={{ maxWidth: '1200px' }}
               className="navbar container mx-auto"
